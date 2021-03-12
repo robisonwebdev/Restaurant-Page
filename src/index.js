f9e95ba3916dae4b254b84b7cfcc4fc351890181
David Robison <david@robisonweb.dev>
2021-03-12 09:08:35 -0700

    Append navbar to content

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ import home from './home';
 import menu from './menu';
 import about from './about';
 import footer from './footer';
+
+const content = document.querySelector('#content');
+
+content.appendChild(navbar);
