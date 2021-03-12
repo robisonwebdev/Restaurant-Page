aabcf1714c0065cd981e2da202e88ba6589edda9
David Robison <david@robisonweb.dev>
2021-03-12 09:04:21 -0700

    Set attributes on variables

--- src/navbar.js
+++ src/navbar.js
@@ -3,3 +3,9 @@ const ul = document.createElement('ul');
 const home = document.createElement('li');
 const menu = document.createElement('li');
 const about = document.createElement('li');
+
+navbar.setAttribute('id', 'navbar');
+ul.setAttribute('id', 'linkContainer');
+home.setAttribute('id', 'homeLink');
+menu.setAttribute('id', 'menuLink');
+about.setAttribute('id', 'aboutLink');
