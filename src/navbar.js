a85528682aef252180fbe7c60af5e27bfd0c0274
David Robison <david@robisonweb.dev>
2021-03-12 09:03:38 -0700

    Set variables to create elements

---
+++ src/navbar.js
@@ -0,0 +1,5 @@
+const navbar = document.createElement('div');
+const ul = document.createElement('ul');
+const home = document.createElement('li');
+const menu = document.createElement('li');
+const about = document.createElement('li');
