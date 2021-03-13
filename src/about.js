7c174109b5a8e3057c6f00c0ba99cb74352451d9
David Robison <david@robisonweb.dev>
2021-03-13 08:48:04 -0700

    Added section title

--- src/about.js
+++ src/about.js
@@ -1,5 +1,6 @@
 const about = document.createElement('div');
 const image = document.createElement('img');
+const sectionTitle = createElement('h2');
 const info = document.createElement('p');
 
 about.setAttribute('id', 'about');
@@ -7,9 +8,11 @@ image.setAttribute('id', 'redBresaola');
 image.setAttribute('src', './images/redBresaola1.png');
 image.setAttribute('alt', 'Red Bresaola Title with red cow head below');
 
+sectionTitle.innerText = 'ABOUT';
 info.innerText = 'Nullam condimentum nunc sed enim efficitur feugiat eu vel lacus. Fusce ut lobortis ante. Quisque ac posuere nibh. Nam tempus orci eget ante venenatis, in tincidunt felis laoreet. Cras mollis massa ante, a vehicula enim iaculis ac. Ut sed imperdiet enim. Nam ut iaculis metus. Nulla sapien lacus, facilisis vitae blandit sit amet, ultrices in libero. Integer mollis tellus urna, a euismod lorem bibendum fringilla. Vivamus sed felis ut tortor lacinia ornare. Integer sed vehicula ante. Vestibulum sodales tincidunt nisi, quis pulvinar elit pulvinar at.'
     
 about.appendChild(image);
+about.appendChild(sectionTitle);
 about.appendChild(info);
 
 export default about;
