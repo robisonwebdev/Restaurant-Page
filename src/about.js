fc8825ee402ad252836d0291f243f395148dc406
David Robison <david@robisonweb.dev>
2021-03-13 08:42:17 -0700

    Changed image src path

--- src/about.js
+++ src/about.js
@@ -4,14 +4,12 @@ const info = document.createElement('p');
 
 about.setAttribute('id', 'about');
 image.setAttribute('id', 'redBresaola');
+image.setAttribute('src', './images/redBresaola1.png');
+image.setAttribute('alt', 'Red Bresaola Title with red cow head below');
 
-info.innerText = 
-    `Nullam condimentum nunc sed enim efficitur feugiat eu vel lacus. Fusce ut lobortis ante. Quisque ac posuere nibh. 
-    Nam tempus orci eget ante venenatis, in tincidunt felis laoreet. Cras mollis massa ante, a vehicula enim iaculis ac. 
-    Ut sed imperdiet enim. Nam ut iaculis metus. Nulla sapien lacus, facilisis vitae blandit sit amet, ultrices in libero. 
-    Integer mollis tellus urna, a euismod lorem bibendum fringilla. Vivamus sed felis ut tortor lacinia ornare. Integer sed vehicula ante. 
-    Vestibulum sodales tincidunt nisi, quis pulvinar elit pulvinar at.`
-
+info.innerText = 'Nullam condimentum nunc sed enim efficitur feugiat eu vel lacus. Fusce ut lobortis ante. Quisque ac posuere nibh. Nam tempus orci eget ante venenatis, in tincidunt felis laoreet. Cras mollis massa ante, a vehicula enim iaculis ac. Ut sed imperdiet enim. Nam ut iaculis metus. Nulla sapien lacus, facilisis vitae blandit sit amet, ultrices in libero. Integer mollis tellus urna, a euismod lorem bibendum fringilla. Vivamus sed felis ut tortor lacinia ornare. Integer sed vehicula ante. Vestibulum sodales tincidunt nisi, quis pulvinar elit pulvinar at.'
+    
+about.appendChild(image);
 about.appendChild(info);
 
 export default about;
