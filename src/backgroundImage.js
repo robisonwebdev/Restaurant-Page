8dcaa3b4a2b3279054b78872c0b26ab916a4ac2e
David Robison <david@robisonweb.dev>
2021-03-15 08:45:09 -0600

    removed backgroundImage from project

--- src/backgroundImage.js
+++
@@ -1,9 +0,0 @@
-const backgroundImage = document.createElement('div');
-const image = document.createElement('img');
-
-backgroundImage.setAttribute('id', 'backgroundImage');
-image.setAttribute('src', './images/backgroundImage.jpg');
-
-backgroundImage.appendChild(image);
-
-export default backgroundImage;
