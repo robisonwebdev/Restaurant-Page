c9e611544737ca870c3b47831a93661b659d1759
David Robison <david@robisonweb.dev>
2021-03-16 17:57:37 -0600

    Created initial function to build menu

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,23 @@
-function menu (title, description, price) {
-    
+function menu (titleValue, descriptionValue, priceValue) {
+    const menu = document.createElement('div');
+    const title = document.createElement('h2');
+    const description = document.createElement('p');
+    const price = document.createElement('h3');
+
+    menu.setAttribute('id', 'menu');
+    title.setAttribute('class', 'menuTitle');
+    description.setAttribute('class', 'menuDescription');
+    price.setAttribute('class', 'menuPrice');
+
+    title.innerText = titleValue;
+    description.innerText = descriptionValue;
+    price.innerText = priceValue;
+
+    menu.appendChild(title);
+    menu.appendChild(description);
+    menu.appendChild(price);
+
+    return menu;
 }
+
+export default menu;
