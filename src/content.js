c95dc00259a7809794f66f5be11923a6f379b9ef
David Robison <david@robisonweb.dev>
2021-03-16 18:12:52 -0600

    Set ID attribute for home, menu, and about

--- src/content.js
+++ src/content.js
@@ -8,9 +8,13 @@ const menuDiv = document.createElement('div');
 const aboutDiv = document.createElement('div');
 
 contentDiv.setAttribute('id', 'content');
+homeDiv.setAttribute('id', 'home');
+menuDiv.setAttribute('id', 'menu');
+aboutDiv.setAttribute('id', 'about');
 
 homeDiv.appendChild(home);
 menuDiv.appendChild(menu('Pizza', 'Best pizza ever!', '$12'));
+menuDiv.appendChild(menu('Donut', 'Very Round', '$2.50'));
 aboutDiv.appendChild(about);
 
 contentDiv.appendChild(homeDiv);
