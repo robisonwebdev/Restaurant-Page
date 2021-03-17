af5743c87b37d788a394430a0678d77835724b95
David Robison <david@robisonweb.dev>
2021-03-16 18:22:05 -0600

    Removed homeDiv and aboutDiv form code

--- src/content.js
+++ src/content.js
@@ -3,22 +3,16 @@ import menu from './menu';
 import about from './about';
 
 const contentDiv = document.createElement('div');
-const homeDiv = document.createElement('div');
 const menuDiv = document.createElement('div');
-const aboutDiv = document.createElement('div');
 
 contentDiv.setAttribute('id', 'content');
-homeDiv.setAttribute('id', 'home');
 menuDiv.setAttribute('id', 'menu');
-aboutDiv.setAttribute('id', 'about');
 
-homeDiv.appendChild(home);
 menuDiv.appendChild(menu('Pizza', 'Best pizza ever!', '$12'));
 menuDiv.appendChild(menu('Donut', 'Very Round', '$2.50'));
-aboutDiv.appendChild(about);
 
-contentDiv.appendChild(homeDiv);
+contentDiv.appendChild(home);
 contentDiv.appendChild(menuDiv);
-contentDiv.appendChild(aboutDiv);
+contentDiv.appendChild(about);
 
 export default contentDiv;
