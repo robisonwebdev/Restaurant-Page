ad67d7bc5b70e59079e7d6ad5cbae4f427b424c1
David Robison <david@robisonweb.dev>
2021-03-16 18:46:50 -0600

    Added back homeDiv and aboutDiv

--- src/content.js
+++ src/content.js
@@ -3,16 +3,23 @@ import menu from './menu';
 import about from './about';
 
 const contentDiv = document.createElement('div');
+const homeDiv = document.createElement('div');
 const menuDiv = document.createElement('div');
+const aboutDiv = document.createElement('div');
 
 contentDiv.setAttribute('id', 'content');
+homeDiv.setAttribute('id', 'home');
 menuDiv.setAttribute('id', 'menu');
+aboutDiv.setAttribute('id', 'about');
+
+homeDiv.appendChild(home);
+aboutDiv.appendChild(about);
 
 menuDiv.appendChild(menu('Pizza', 'Best pizza ever!', '$12'));
 menuDiv.appendChild(menu('Donut', 'Very Round', '$2.50'));
 
-contentDiv.appendChild(home);
+contentDiv.appendChild(homeDiv);
 contentDiv.appendChild(menuDiv);
-contentDiv.appendChild(about);
+contentDiv.appendChild(aboutDiv);
 
 export default contentDiv;
