a986fd1ee3c4d9a0614eefabe51192352f0f82c4
David Robison <david@robisonweb.dev>
2021-03-18 09:13:26 -0600

    Added menu item descriptions

--- src/content.js
+++ src/content.js
@@ -10,8 +10,12 @@ contentDiv.setAttribute('id', 'content');
 menuDiv.setAttribute('id', 'menu');
 
 menuDiv.appendChild(logo());
-menuDiv.appendChild(menu('Pizza', 'Best pizza ever!', '$12'));
-menuDiv.appendChild(menu('Donut', 'Very Round', '$2.50'));
+menuDiv.appendChild(menu('Picanha Porchetta Pork', 'Esse laborum id, non venison fugiat nisi cupidatat excepteur quis chislic boudin enim. Laborum qui nisi beef aliqua alcatra.', '$12'));
+menuDiv.appendChild(menu('Sirloin Magna', ' Non tenderloin ex, capicola turducken id fatback labore drumstick adipisicing short loin.', '$22'));
+menuDiv.appendChild(menu('Turducken Aliquip', ' In short loin burgdoggen swine pancetta ham turkey minim culpa consectetur tenderloin flank. Tempor magna ut nisi burgdoggen kevin eu aliquip kielbasa leberkas pork.', '$8'));
+menuDiv.appendChild(menu('Tri-tip Aliqua Salami Spareribs', 'Voluptate filet mignon kevin reprehenderit veniam meatloaf mollit aliqua ad labore do ut dolor.', '$13'));
+menuDiv.appendChild(menu('Shankle Chicken Frankfurter', 'Rump quis frankfurter labore pork pancetta do buffalo aute meatloaf landjaeger jerky spare ribs.', '$9'));
+menuDiv.appendChild(menu('Kielbasa Jerky Laborum', ' Tenderloin sunt landjaeger doner labore laborum bacon.', '$3'));
 
 contentDiv.appendChild(home);
 contentDiv.appendChild(menuDiv);
