ee4e4bb6c9ae46a5965f6189801975f16f915072
David Robison <david@robisonweb.dev>
2021-03-18 10:23:36 -0600

    Moved navbar code into function named navbar

--- src/navbar.js
+++ src/navbar.js
@@ -1,23 +1,27 @@
-const navbar = document.createElement('div');
-const ul = document.createElement('ul');
-const home = document.createElement('li');
-const menu = document.createElement('li');
-const about = document.createElement('li');
+function navbar() {
+    const navbar = document.createElement('div');
+    const ul = document.createElement('ul');
+    const home = document.createElement('li');
+    const menu = document.createElement('li');
+    const about = document.createElement('li');
+    
+    navbar.setAttribute('id', 'navbar');
+    ul.setAttribute('id', 'linkContainer');
+    home.setAttribute('id', 'homeLink');
+    menu.setAttribute('id', 'menuLink');
+    about.setAttribute('id', 'aboutLink');
+    
+    home.innerText = 'HOME';
+    menu.innerText = 'MENU';
+    about.innerText = 'ABOUT';
+    
+    ul.appendChild(home);
+    ul.appendChild(menu);
+    ul.appendChild(about);
+    
+    navbar.appendChild(ul);
 
-navbar.setAttribute('id', 'navbar');
-ul.setAttribute('id', 'linkContainer');
-home.setAttribute('id', 'homeLink');
-menu.setAttribute('id', 'menuLink');
-about.setAttribute('id', 'aboutLink');
-
-home.innerText = 'HOME';
-menu.innerText = 'MENU';
-about.innerText = 'ABOUT';
-
-ul.appendChild(home);
-ul.appendChild(menu);
-ul.appendChild(about);
-
-navbar.appendChild(ul);
+    return navbar;
+}
 
 export default navbar;
