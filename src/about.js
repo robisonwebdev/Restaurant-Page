5472b77d71e720f170d2412e5cd6035873508d07
David Robison <david@robisonweb.dev>
2021-03-18 08:23:35 -0600

    Imported logo.js and appened to about. Remove old image code

--- src/about.js
+++ src/about.js
@@ -1,3 +1,5 @@
+import logo from './logo';
+
 const about = document.createElement('div');
 const image = document.createElement('img');
 const sectionTitle = document.createElement('h2');
@@ -5,14 +7,11 @@ const info = document.createElement('p');
 
 about.setAttribute('id', 'about');
 about.setAttribute('class', 'containerSections');
-image.setAttribute('class', 'redBresaola');
-image.setAttribute('src', './images/redBresaola1.png');
-image.setAttribute('alt', 'Red Bresaola Title with red cow head below');
 
 sectionTitle.innerText = 'ABOUT';
 info.innerText = 'Nullam condimentum nunc sed enim efficitur feugiat eu vel lacus. Fusce ut lobortis ante. Quisque ac posuere nibh. Nam tempus orci eget ante venenatis, in tincidunt felis laoreet. Cras mollis massa ante, a vehicula enim iaculis ac. Ut sed imperdiet enim. Nam ut iaculis metus. Nulla sapien lacus, facilisis vitae blandit sit amet, ultrices in libero. Integer mollis tellus urna, a euismod lorem bibendum fringilla. Vivamus sed felis ut tortor lacinia ornare. Integer sed vehicula ante. Vestibulum sodales tincidunt nisi, quis pulvinar elit pulvinar at.'
     
-about.appendChild(image);
+about.appendChild(logo());
 about.appendChild(sectionTitle);
 about.appendChild(info);
 
