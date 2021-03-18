5f16cbefea7ec0420dbdaddccf57e2e39400c0c1
David Robison <david@robisonweb.dev>
2021-03-18 10:17:09 -0600

    Put home code into a home function

--- src/home.js
+++ src/home.js
@@ -1,18 +1,22 @@
 import logo from './logo';
 
-const home = document.createElement('div');
-const description = document.createElement('p');
-const menuBtn = document.createElement('button');
+function home() {
+    const home = document.createElement('div');
+    const description = document.createElement('p');
+    const menuBtn = document.createElement('button');
 
-home.setAttribute('id', 'home');
-home.setAttribute('class', 'containerSections');
-menuBtn.setAttribute('id', 'menuBtn');
+    home.setAttribute('id', 'home');
+    home.setAttribute('class', 'containerSections');
+    menuBtn.setAttribute('id', 'menuBtn');
 
-description.innerText = 'Scelerisque varius morbi enim nunc faucibus a pellentesque sit.';
-menuBtn.innerText = 'Menu';
+    description.innerText = 'Scelerisque varius morbi enim nunc faucibus a pellentesque sit.';
+    menuBtn.innerText = 'Menu';
 
-home.appendChild(logo());
-home.appendChild(description);
-home.appendChild(menuBtn);
+    home.appendChild(logo());
+    home.appendChild(description);
+    home.appendChild(menuBtn);
+
+    return home;
+}
 
 export default home;
