7b298ce373d8c46dbf5bf83cb635d58c9410e5f6
David Robison <david@robisonweb.dev>
2021-03-18 11:29:53 -0600

    Added button type button

--- src/home.js
+++ src/home.js
@@ -8,6 +8,7 @@ function home() {
     home.setAttribute('id', 'home');
     home.setAttribute('class', 'containerSections');
     menuBtn.setAttribute('id', 'menuBtn');
+    menuBtn.setAttribute('type', 'button');
 
     description.innerText = 'Scelerisque varius morbi enim nunc faucibus a pellentesque sit.';
     menuBtn.innerText = 'Menu';
