ead1355c8be4c5c58eda00d060332608a7a408ee
David Robison <david@robisonweb.dev>
2021-03-18 08:52:07 -0600

    Added blank div with class of borderBottom and append to menuItem

--- src/menu.js
+++ src/menu.js
@@ -3,11 +3,13 @@ function menu (titleValue, descriptionValue, priceValue) {
     const title = document.createElement('h2');
     const description = document.createElement('p');
     const price = document.createElement('h3');
+    const borderBottom = document.createElement('div');
 
     menuItem.setAttribute('class', 'menuItem');
     title.setAttribute('class', 'menuTitle');
     description.setAttribute('class', 'menuDescription');
     price.setAttribute('class', 'menuPrice');
+    borderBottom.setAttribute('class', 'borderBottom');
 
     title.innerText = titleValue;
     description.innerText = descriptionValue;
@@ -16,6 +18,7 @@ function menu (titleValue, descriptionValue, priceValue) {
     menuItem.appendChild(title);
     menuItem.appendChild(description);
     menuItem.appendChild(price);
+    menuItem.appendChild(borderBottom);
 
     return menuItem;
 }
