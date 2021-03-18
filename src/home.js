1c9d3ddf490833344e3957aef4fa01febb77639e
David Robison <david@robisonweb.dev>
2021-03-18 08:26:02 -0600

    Imported logo.js and appended it to home. Removed old image code

--- src/home.js
+++ src/home.js
@@ -1,19 +1,17 @@
+import logo from './logo';
+
 const home = document.createElement('div');
-const image = document.createElement('img');
 const description = document.createElement('p');
 const menuBtn = document.createElement('button');
 
 home.setAttribute('id', 'home');
 home.setAttribute('class', 'containerSections');
-image.setAttribute('class', 'redBresaola');
-image.setAttribute('src', './images/redBresaola1.png');
-image.setAttribute('alt', 'Red Bresaola Title with red cow head below');
 menuBtn.setAttribute('id', 'menuBtn');
 
 description.innerText = 'Scelerisque varius morbi enim nunc faucibus a pellentesque sit.';
 menuBtn.innerText = 'Menu';
 
-home.appendChild(image);
+home.appendChild(logo());
 home.appendChild(description);
 home.appendChild(menuBtn);
 
